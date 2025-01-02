ed5d0bdbd0a025eae5fd23ba27040dc5dcaba9f0
KotaChien <KotaChien@transglobe.com.tw>
2025-01-02 12:07:51 +0800

    fix path

--- support/CAPTCHAsolver.js
+++ support/CAPTCHAsolver.js
@@ -5,10 +5,6 @@
 
 import fs from 'fs'
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 export async function solver(page) {
   const model_text = fs.readFileSync(path.join(__dirname, 'cbl-js', 'model.txt'), 'utf8')
